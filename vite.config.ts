b3ee419bf3f9f149119e60e4a03f6fe35a129bbf
Jami Virtanen <jami.virtanen@vincit.fi>
2025-04-22 15:53:53 +0300

    Fix vite conf after repo name change

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/ShotGobbler/",
+  base: "/shot-gobbler/",
 });
